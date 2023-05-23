e8b179df627759620d9291fb7ae77850a50f4da5
V <>
2023-05-23 15:25:35 +0800

    1.0.0

--- Example/MediaEditorManager/ViewController.swift
+++ Example/MediaEditorManager/ViewController.swift
@@ -6,19 +6,100 @@
 //  Copyright (c) 2023 oauth2. All rights reserved.
 //
 
+/*
+ https://tenloy.github.io/2022/03/18/avfoundation.html
+ 
+ https://developer.apple.com/documentation/avfoundation/video_settings
+ https://developer.apple.com/documentation/avfoundation/audio_settings
+  
+ */
+
+/*
+ videoTrack.formatDescriptions 配置
+ 
+ AVAssetWriterInputPixelBufferAdaptor 将视频样本追加到资产写入器输入的对象
+ */
+
+
 import UIKit
+import MediaEditorManager
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let asset = AVAsset(url: URL(string: "")!)
+        
+        let manager = MediaEditorManager()
+        manager.changedAVType(type: .mov)
+        manager.changedAVExportPreset(type: .highest)
+        
+        //旋转
+        manager.correctingVideo(at: asset) { result in
+            switch result {
+            case .success(let url):
+                break
+            case .failure(let error):
+                break
+            }
+        }
+        
+        //格式转换
+        manager.transformFormat(at: asset, to: .mov) { result in
+            
+        }
+        
+        //裁剪
+        manager.tailoringVideo(at: asset, CMTimeRange.zero) { result in
+            
+        }
+        manager.tailoringVideo(at: asset, tailoring: MediaEditorTailoring(star: 0, end: 1)) { result in
+            
+        }
+        
+        //拼接
+        manager.combinedGrowthVideo(by: [asset]) { result in
+            
+        }
+        
+        //水印元数据
+        var waterMaker = MediaEditorWaterMaker(image: UIImage(), centerOffsetX: 0, centerOffsetY: 0, star: .zero, duration: 1)
+        //添加关键帧动画
+        waterMaker.addAnimation(keyPath: "", values: [], keyTimes: [])
+        
+        //水印
+        manager.waterMaker(at: asset, waterMakers: [waterMaker]) { result in
+            
+        }
+        
+        //视频压缩
+        let video = [
+            AVVideoCodecKey: AVVideoCodecType.h264,
+            AVVideoWidthKey: 1080,
+            AVVideoHeightKey: 760,
+            AVVideoCompressionPropertiesKey: [
+                AVVideoAverageBitRateKey: 960000, //比特率
+                AVVideoExpectedSourceFrameRateKey: 24, //帧
+                AVVideoProfileLevelKey: AVVideoProfileLevelH264High40
+            ] as [String : Any]
+        ] as [String : Any]
+        
+        let audio = [
+            AVFormatIDKey: kAudioFormatMPEG4AAC,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100,
+            AVEncoderBitRateKey: 128000
+        ]
+        
+        let manager2 = MediaDataEditorManager()
+        manager2.changedAVType(type: .mov)
+        
+        manager2.edit(at: asset, tailoring: nil, compression: nil, waterMakers: nil) { result in
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
--- MediaEditorManager.podspec
+++ MediaEditorManager.podspec
@@ -8,27 +8,22 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MediaEditorManager'
-  s.version          = '0.1.0'
+  s.version          = '1.0.0'
   s.summary          = 'A short description of MediaEditorManager.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/oauth2/MediaEditorManager'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.homepage         = 'https://github.com/CXgfh'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'oauth2' => 'gfh.cynthia@icloud.com' }
-  s.source           = { :git => 'https://github.com/oauth2/MediaEditorManager.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/CXgfh/MediaEditorManager', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '10.0'
+  s.ios.deployment_target = '11.0'
+  
+  s.swift_versions = '5.0'
 
   s.source_files = 'MediaEditorManager/Classes/**/*'
   
